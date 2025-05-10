5791783fabf819f52bbeb6e9dccc437a20ebc478
MarwenOnGit <marwenbenahmed89@gmail.com>
2025-05-10 18:10:25 +0100

    new String Comparator

---
+++ src/comparateurs/ComparateurCombine.java
@@ -0,0 +1,28 @@
+package comparateurs;
+
+import inputs.Nom;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Arrays;
+public class ComparateurCombine implements ComparateurDeNom{
+    public double comparerNom ( Nom nom1 , Nom nom2 ){
+        ComparateurLevenshtein comparateurLev = new ComparateurLevenshtein();
+        List<Double> listeFinaleDeScore = new ArrayList<>();
+        for ( String n1 : nom1.getMots()){
+        List<Double> listeDeScores = new ArrayList<Double>();
+            for ( String n2 : nom2.getMots() ){
+                listeDeScores.add (comparateurLev.comparer(n1,n2));
+            }
+            listeFinaleDeScore.add (Collections.max(listeDeScores));
+        }
+        double valFinale = 0.0 ;
+        for ( double d : listeFinaleDeScore ){
+            valFinale += d;
+        }
+        return (double) valFinale/listeFinaleDeScore.size();
+
+    }
+
+}
